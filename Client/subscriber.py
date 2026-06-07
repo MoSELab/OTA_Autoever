c8223f3a4955322047bcc60d132ba7b6d2746f29
l-zzzzz <gpfvmal2159@gmail.com>
2026-06-07 16:13:45 +0900

    Update subscriber.py

--- Client/subscriber.py
+++ Client/subscriber.py
@@ -5,7 +5,7 @@ import os
 def on_connect(client, userdata, flags, reasonCode):
     if reasonCode == 0:
         print("Connected successfully.")
-        client.subscribe(topic)
+        client.subscribe(TOPIC)
     else:
         print(f"Failed to connect, return code {reasonCode}")
 
@@ -32,12 +32,14 @@ def topic_subscribe():
 
     #MQTT broker를 통해 지정 Topic 구독 및 메시지 수신 대기상태로 전환
     try:
-        client.connect(broker_ip, port, keepalive = 60)
+        client.connect(BROKER_IP, BROKER_PORT, keepalive = 60)
         client.loop_forever()
+    except Exception as e:
+        print(f"Connection error {e}")
 
 #해당 코드를 직접 실행할 때만 하기 코드가 동작하도록 작성!
 if __name__ == "__main__":
     TOPIC = '<topic>' #Message 수신 주제를 정의하는 변수
     BROKER_IP = "<broker ip>" #Message publish를 중개해주는 MQTT Broker의 주소
-    BROKER_PORT - "<broker port>" #연결하려는 MQTT Broker의 open port
+    BROKER_PORT = "<broker port>" #연결하려는 MQTT Broker의 open port
     topic_subscribe()
